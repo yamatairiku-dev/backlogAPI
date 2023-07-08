ed5bac7f125cc46c83a8d68a7405f868cd528524
yamatairiku-dev <yamatairiku.dev@gmail.com>
2023-07-08 21:54:43 +0900

    改良

--- getUsersNotLoggedIn.js
+++ getUsersNotLoggedIn.js
@@ -11,11 +11,11 @@ const standardDate = now - 180 * 24 * 60 * 60 * 1000 // 180日前
 const asyncFunc = async () => {
   const result = await axios.get(`${process.env.MY_SPACE}${getUsersUrl}?apiKey=${process.env.API_KEY}`)
     .catch(() => console.log('エラー'))
-  const userNotLoggedIn = result.data.map(e => [e.id, e.name, e.mailAddress, new Date(e.lastLoginTime)]).filter(val => (val[3].getTime() <= standardDate))
+  const usersNotLoggedIn = result.data.map(e => [e.id, e.name, e.mailAddress, new Date(e.lastLoginTime)]).filter(val => (val[3].getTime() <= standardDate))
   console.log('全ユーザー数: ', result.data.length)
-  console.log('しばらくログインしていないユーザー数: ', userNotLoggedIn.length)
+  console.log('しばらくログインしていないユーザー数: ', usersNotLoggedIn.length)
   let usersNotLoggedInCSV = ''
-  userNotLoggedIn.map(e => (usersNotLoggedInCSV += e.join(',') + '\n'))
+  usersNotLoggedIn.map(e => (usersNotLoggedInCSV += e.join(',') + '\n'))
   fs.writeFileSync(`${output}/usersNotLoggedIn.csv`, usersNotLoggedInCSV)
 }
 
