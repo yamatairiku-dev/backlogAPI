6ee51950c3ba2fa2e4be252ae61eb9cfa21e7f0b
yamatairiku-dev <yamatairiku.dev@gmail.com>
2024-01-29 21:23:16 +0900

    長期間ログインしていないユーザー情報にプロジェクト名を付加

--- Readme.md
+++ Readme.md
@@ -1,43 +1,82 @@
 # 事前準備
+
 ## 環境設定ファイル
+
 環境変数を設定するための '.env' ファイルを作成
+
 ```
 touch .env
 ```
+
 ## 環境変数
+
 環境変数を設定
+
 ```
 MY_SPACE = 'スペースのURL（末尾のスラッシュは無し）'
 API_KEY = '管理者権限のAPIキー'
 EXCLUSION_PROJECTS = ['ユーザー取得を除外するプロジェクトのIDの配列']
 NOT_LOGGED_IN_DAYS = 最近ログインしていない期間（日数を数字で）
 ```
+
 ## 出力フォルダー
+
 ファイルの出力先フォルダーを作成
+
 ```
 mkdir output
 ```
+
 # 一括処理
+
 ## getUsersToBeDeleted.js
+
 ### 機能
+
 プロジェクトに参加していないユーザーを出力する　=> usersToBeDeleted.csv
+
 ## getUsersNotLoggedIn.js
+
 ### 機能
+
 しばらくログインしていないユーザーを出力する　=> getUsersNotLoggedIn.csv
-# 分割処理
+
+## getUsersNotLoggedInWithPjInfo.js
+
+### 機能
+
+しばらくログインしていないユーザーを出力する（プロジェクト名付き、延べユーザー）=> usersToBeDeletedWithPjInfo.csv
+
+# 分割処理（一括処理の途中経過が必要なときにご利用ください）
+
 ## 01getUsers.js
+
 ### 機能
+
 ユーザー一覧を出力する　=> userIDs.json
+
 ## 02getProjectIDs.js
+
 ### 機能
+
 プロジェクト一覧を出力する => projectIDs.json
+
 ## 03getPjMember.js
+
 ### 前提条件
-02getProjectIDs.jsが実行済み
+
+02getProjectIDs.js が実行済み
+
 ### 機能
+
 プロジェクトに参加しているユーザー一覧を出力する => pjMember.json, pjMember.csv
+
 ## 04usersForDelete.js
+
 ### 前提条件
-01getUsers.js, 03getPjMember.jsが実行済み
+
+01getUsers.js, 03getPjMember.js が実行済み
+
 ### 機能
+
 プロジェクトに参加していないユーザーを出力する　=> usersForDelete.csv
---
+++ getUsersNotLoggedInWithPjInfo.js
@@ -0,0 +1,89 @@
+"use strict";
+const startTime = Date.now();
+const axios = require("axios");
+const fs = require("fs");
+const output = "./output";
+require("dotenv").config();
+
+// ユーザー取得を除外するプロジェクト
+const exclusionProjects = process.env.EXCLUSION_PROJECTS;
+
+// 全ユーザーの情報を取得(プロミス)
+// const allUsers = axios.get(
+//   `${process.env.MY_SPACE}/api/v2/users?apiKey=${process.env.API_KEY}`
+// );
+
+// 全プロジェクトの情報を取得(プロミス)
+const allProjects = axios.get(
+  `${process.env.MY_SPACE}/api/v2/projects?apiKey=${process.env.API_KEY}&archived=false&all=true`
+);
+
+// プロジェクトのユーザーを取得する関数
+const usersPerProject = (projectID) => {
+  return axios.get(
+    `${process.env.MY_SPACE}/api/v2/projects/${projectID}/users?apiKey=${process.env.API_KEY}`
+  );
+};
+
+// 全プロジェクトのユーザー情報を取得
+allProjects
+  .then((result) => {
+    const projectKeys = result.data
+      .map((e) => e.projectKey) // プロジェクトの情報からプロジェクトID（key）を取得
+      .filter((val) => !exclusionProjects.includes(val)); // 環境変数で指定したプロジェクトIDを除外
+    console.log("プロジェクト数: ", projectKeys.length);
+    // プロジェクト情報一覧
+    const projectNames = result.data.map((e) => [e.projectKey, e.name]);
+
+    // プロジェクト毎のユーザーを取得(プロミス)
+    const totalUsers = projectKeys.map((e) => usersPerProject(e));
+    // 全プロジェクトのユーザー情報の取得が完了したら
+    Promise.all(totalUsers).then((result) => {
+      const usersByPJ = []; // PJごとのUser情報を格納するための配列
+      result.map((e) => usersByPJ.push(e.data));
+      const usersWithPjInfo = []; // PJ情報を付加したユーザー一覧
+      for (let i = 0; i < projectKeys.length; i++) {
+        usersByPJ[i].map((e) =>
+          usersWithPjInfo.push([
+            e.id,
+            e.name,
+            e.mailAddress,
+            new Date(e.lastLoginTime),
+            projectKeys[i],
+            projectNames.find((e) => e[0] === projectKeys[i])[1],
+          ])
+        );
+      }
+
+      const now = Date.now();
+      const standardDate =
+        now - process.env.NOT_LOGGED_IN_DAYS * 24 * 60 * 60 * 1000;
+      const usersNotLoggedIn = usersWithPjInfo
+        .filter((val) => val[3].getTime() <= standardDate)
+        .sort((a, b) => {
+          if (a[0] > b[0]) {
+            return 1;
+          } else {
+            return -1;
+          }
+        });
+      // console.log(usersNotLoggedIn);
+      console.log(
+        `${process.env.NOT_LOGGED_IN_DAYS}日間ログインしていない延べユーザー数: `,
+        usersNotLoggedIn.length
+      );
+      // CSVファイルとして出力
+      let getUsersNotLoggedInWithPjInfoCSV = "";
+      usersNotLoggedIn.map(
+        (e) => (getUsersNotLoggedInWithPjInfoCSV += e.join(",") + "\n")
+      );
+      fs.writeFileSync(
+        `${output}/usersToBeDeletedWithPjInfo.csv`,
+        getUsersNotLoggedInWithPjInfoCSV
+      );
+      console.log("done");
+      const endTime = Date.now();
+      console.log("実行時間（ミリ秒）: ", endTime - startTime);
+    });
+  })
+  .catch((err) => console.log("エラー", err));
